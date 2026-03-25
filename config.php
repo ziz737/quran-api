de0e8b34c229edcfabbe4af336873240aa89b79c
ziz737 <ziz737@gmail.com>
2026-03-25 18:17:25 +0300

    Add files via upload

--- config.php
+++ config.php
@@ -5,7 +5,7 @@
 
 // Connection string من MongoDB Atlas
 // مثال: mongodb+srv://user:pass@cluster0.xxxxx.mongodb.net/
-define('MONGO_URI',    getenv('MONGO_URI') ?: 'mongodb+srv://quran_test:9aKzsIUstc1kQ7yf@ziz737.rukv0uk.mongodb.net/?appName=ziz737');
+define('MONGO_URI',    getenv('MONGO_URI') ?: 'mongodb+srv://quran_test:9aKzsIUstc1kQ7yf@ziz737.rukv0uk.mongodb.net/?appName=qurantest');
 
 // اسم قاعدة البيانات
 define('MONGO_DB',     getenv('MONGO_DB')  ?: 'quran_test');
